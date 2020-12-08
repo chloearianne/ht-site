7504ec59ae14ae870ef60071f752a6dd6e2cf687
Chloe Lim <chloe@wearethorn.org>
2020-12-07 21:16:37 -0800

    Initialize

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.static("images"));
+
+var things = require('./things.js');
+app.use('/', things);
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+
+console.debug('Server listening on port ' + port);
