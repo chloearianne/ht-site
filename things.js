0ea0a96c352fee861ef29d789803c0985007e9d8
Chloe Lim <chloe@wearethorn.org>
2020-12-19 20:47:25 -0800

    change stuff

--- things.js
+++ things.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/main.html'));
+    res.sendFile(path.join(__dirname+'/murp.html'));
 });
 
 module.exports = router;
